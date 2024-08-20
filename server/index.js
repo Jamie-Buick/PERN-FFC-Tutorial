f06c0006ae7b89046429cdf3d8aedd93b92c2a1f
Jamie Buick <jamiebuick28@gmail.com>
2024-08-20 22:01:47 +0100

    gitignore update, indexjs routes create, get all, get 1 have been set up

--- server/index.js
+++ server/index.js
@@ -4,6 +4,8 @@ import db from "./db.js";
 
 
 const app = express();
+app.use(express.json()); // This middleware is necessary to parse JSON bodies
+
 
 
 //middleware
@@ -13,9 +15,51 @@ app.use(cors());
 
 //create a todo
 
+app.post("/todos", async(req, res) => {
+    try 
+    {
+        const { description } = req.body;
+        const newTodo = await db.query(
+            "INSERT INTO todo (description) VALUES ($1) RETURNING * ", 
+            [description]
+        );  
+        res.json(newTodo.rows[0]);
+    } 
+    catch (err) 
+    {
+        console.log(err.message);
+    }
+});
+
 //Get all todos
 
+app.get("/todos", async(req, res) => {
+    try 
+    {
+        const allTodos = await db.query("SELECT * FROM todo");
+        res.json(allTodos.rows);
+    } 
+    catch (err) 
+    {
+        console.log(err.message);
+    }
+});
+
 //Get a todo
+
+app.get("/todos/:id", async(req, res) => {
+    try 
+    {
+        //console.log(req.params);
+        const {id} = req.params;
+        const todo = await db.query("SELECT * FROM todo WHERE todo_id = $1", [id]);
+        res.json(todo.rows[0]);
+    } 
+    catch (err) 
+    {
+        console.log(err.message);
+    }
+});
 
 //Update a todo
 

4e2ba4ed206158a42b331ca6c197c46e9f5039f5
Jamie Buick <jamiebuick28@gmail.com>
2024-08-20 21:31:37 +0100

    SQL database set up, db connect file set up which has been included in gitignore, index.js ready for expressjs routes for crud

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import cors from "cors";
+import db from "./db.js";
 
 
 const app = express();
@@ -8,7 +9,20 @@ const app = express();
 //middleware
 app.use(cors());
 
+//ROUTES//
+
+//create a todo
+
+//Get all todos
+
+//Get a todo
+
+//Update a todo
+
+//Delete a todo
+
 
 app.listen(5000,() => {
     console.log("Server has started on port 5000");
 });
+

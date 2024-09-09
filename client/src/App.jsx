56c0f8dadb4f8ffecc578971f5d965892f22b4e1
Jamie Buick <jamiebuick28@gmail.com>
2024-09-09 22:03:07 +0100

    Navbar component added, login, register and app updated. Login and register pages are working at a basic level with forms, no connection to back end yet

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,24 +1,42 @@
 import './App.css';
 import React, { Fragment } from 'react';
+import {Routes, Route} from 'react-router-dom';
+
+
+// Pages
+import Home from '../pages/Home';
+import Login from '../pages/Login';
+import Register from '../pages/Register'
+
 
 //components
 import InputToDo from "./Components/InputToDo";
 import ListToDos from "./Components/ListToDo";
+import Navbar from './Components/Navbar';
+
+
 
 
 
 
 function App() {
   return (
-    <Fragment>
-      <div className="container">
-        <InputToDo/>
-        <ListToDos/>
+    <>
+      <Navbar />
+
+      <Routes>
+     
+
+        <Route path='/' element={<Home />} />
+        <Route path='/register' element={<Register />} />
+        <Route path='/login' element={<Login />} />
+        {/* <InputToDo /> */}
+        {/* <ListToDos /> */}
        
         
-      </div>
-      
-    </Fragment>
+   
+      </Routes>
+    </>
   );
 }
 
